a32faed2d26ede40d2400269fd2514d1fe6ddbe2
davit77ha1r <davit77ha1r@gmail.com>
2020-11-28 08:16:28 +0400

    all is working

--- online_shop/main/views.py
+++ online_shop/main/views.py
@@ -8,3 +8,13 @@ def index_page(request):
     return render(request, "main/index.html")
 def about(request):
     return render(request, "main/about.html")
+def contact(request):
+    return render(request, "main/contact.html")
+def shoplocator(request):
+    return render(request, "main/shoplocator.html")
+def help(request):
+    return render(request, "main/help.html")
+def privacy(request):
+    return render(request, "main/privacy.html")
+def terms(request):
+    return render(request, "main/terms.html")
